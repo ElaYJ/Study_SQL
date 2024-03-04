fd63525bb36420ddf649ec2bed59385cf5b3ede1
ElaYJ <syunjae21@gmail.com>
2024-03-04 14:52:53 +0900

    2024/03/04

--- coursework/02_gas_brand.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO gas_brand VALUES (1, "SK에너지");
-INSERT INTO gas_brand VALUES (2, "현대오일뱅크");
-INSERT INTO gas_brand VALUES (3, "GS칼텍스");
-INSERT INTO gas_brand VALUES (4, "S-OIL");
-INSERT INTO gas_brand VALUES (5, "알뜰주유소");
-INSERT INTO gas_brand VALUES (6, "자가상표");
